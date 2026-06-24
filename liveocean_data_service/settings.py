f64cb5f601d9c1379f7d618e4660a274bdb31f76
xanderfehsenfeld <fehsenfeld.xander@gmail.com>
2026-06-24 15:41:37 -0700

    Add configuration to support cloud sql

--- liveocean_data_service/settings.py
+++ liveocean_data_service/settings.py
@@ -10,24 +10,81 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import io
 import os
 from pathlib import Path
+from urllib.parse import urlparse
+
+import environ
+import google.auth
+from google.cloud import secretmanager
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zc&2=hgnb3-%skr&%l&m%6=+8cp4_1abd#3ve^dirt-mh2+$we'
-
+# [START cloudrun_django_secret_config]
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Change this to "False" when you are ready for production
+env = environ.Env(DEBUG=(bool, True))
+env_file = os.path.join(BASE_DIR, ".env")
 
-ALLOWED_HOSTS = []
+# Attempt to load the Project ID into the environment, safely failing on error.
+try:
+    _, os.environ["GOOGLE_CLOUD_PROJECT"] = google.auth.default()
+except google.auth.exceptions.DefaultCredentialsError:
+    pass
 
+if os.path.isfile(env_file):
+    # Use a local secret file, if provided
+
+    env.read_env(env_file)
+# # [START_EXCLUDE]
+# elif os.getenv("TRAMPOLINE_CI", None):
+#     # Create local settings if running with CI, for unit testing
+
+#     placeholder = (
+#         f"SECRET_KEY=a\n"
+#         "GS_BUCKET_NAME=None\n"
+#         f"DATABASE_URL=sqlite://{os.path.join(BASE_DIR, 'db.sqlite3')}"
+#     )
+#     env.read_env(io.StringIO(placeholder))
+# # [END_EXCLUDE]
+elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+    # Pull secrets from Secret Manager
+    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+
+    client = secretmanager.SecretManagerServiceClient()
+    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(
+        name=name).payload.data.decode("UTF-8")
+
+    env.read_env(io.StringIO(payload))
+else:
+    raise Exception(
+        "No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+# [END cloudrun_django_secret_config]
+SECRET_KEY = env("SECRET_KEY")
+
+DEBUG = env("DEBUG")
+
+
+# [START cloudrun_django_csrf]
+# SECURITY WARNING: It's recommended that you use this when
+# running in production. The URLs will be known once you first deploy
+# to Cloud Run. This code takes the URLs and converts it to both these settings formats.
+CLOUDRUN_SERVICE_URLS = env("CLOUDRUN_SERVICE_URLS", default=None)
+if CLOUDRUN_SERVICE_URLS:
+    CSRF_TRUSTED_ORIGINS = env("CLOUDRUN_SERVICE_URLS").split(",")
+    # Remove the scheme from URLs for ALLOWED_HOSTS
+    ALLOWED_HOSTS = [urlparse(url).netloc for url in CSRF_TRUSTED_ORIGINS]
+
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+else:
+    ALLOWED_HOSTS = []
+# [END cloudrun_django_csrf]
 
 # Application definition
 
@@ -78,16 +135,35 @@ WSGI_APPLICATION = 'liveocean_data_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+# Database
+# [START cloudrun_django_database_config]
+# Use django-environ to parse the connection string
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.environ.get("PG_DB_NAME"),
-        'USER': os.environ.get("PG_USER"),
-        'PASSWORD': os.environ.get("PG_PSWD"),
-        'HOST': os.environ.get("PG_HOST_DEV" if DEBUG else "PG_HOST"),
-        'PORT': os.environ.get("PG_PORT"),
-    }
+
+
+    "default":
+    env.db(engine='django.contrib.gis.db.backends.postgis')
 }
+
+print(DATABASES['default']["ENGINE"])
+
+
+# If the flag as been set, configure to use proxy
+if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
+    DATABASES["default"]["HOST"] = "127.0.0.1"
+    DATABASES["default"]["PORT"] = 5432
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': os.environ.get("PG_DB_NAME"),
+#         'USER': os.environ.get("PG_USER"),
+#         'PASSWORD': os.environ.get("PG_PSWD"),
+#         'HOST': os.environ.get("PG_HOST_DEV" if DEBUG else "PG_HOST"),
+#         'PORT': os.environ.get("PG_PORT"),
+#     }
+# }
 
 
 # Password validation
@@ -125,6 +201,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# # Static files (CSS, JavaScript, Images)
+# # [START cloudrun_django_static_config]
+# # Define static storage via django-storages[google]
+# GS_BUCKET_NAME = env("GS_BUCKET_NAME")
+# STATIC_URL = "/static/"
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "storages.backends.gcloud.GoogleCloudStorage",
+#     },
+#     "staticfiles": {
+#         "BACKEND": "storages.backends.gcloud.GoogleCloudStorage",
+#     },
+# }
+# GS_DEFAULT_ACL = "publicRead"
+# # [END cloudrun_django_static_config]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
